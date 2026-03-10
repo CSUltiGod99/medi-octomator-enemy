d416c8659868578cac8f9b022bbf636a5fcf7722
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2026-03-10 16:04:31 +0000

    Fix syntax errors in octom.cs

---
+++ octom.cs
@@ -0,0 +1,24 @@
+// Octomator enemy manager:
+using System.Collections;
+using System.Collections.Generic;
+using unityengine:
+
+// Create body:
+public class octom : MonoBehaviour {
+
+
+
+// variables:
+  
+
+    
+   
+    
+    
+    public int flyingTime = 10.0f;
+    public int flyEndTime = 13.0f;
+
+
+
+  
+}

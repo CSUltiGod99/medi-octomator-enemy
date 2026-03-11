18f676c4ab4143ca3f64921ad6c587892bae908f
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2026-03-11 18:49:19 +0000

    Rename class and fix formatting in enemyMovement.cs

---
+++ enemyMovement.cs
@@ -0,0 +1,32 @@
+// Octomator enemy manager:
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine:
+
+// Create body:
+public class enemyMove : MonoBehaviour {
+  // variables: 
+      
+    public int flyingTime = 10.0f;
+    public int flyEndTime = 13.0f;
+
+    
+  
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+
+  }
+  
+  void Animate()
+  {
+
+  }
+  
+
+}

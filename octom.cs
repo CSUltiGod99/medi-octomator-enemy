565b641622683eb1d92dc3c529b0ddb1c7153ce6
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2026-03-11 18:46:08 +0000

    Implement Start and Update methods in octom class
    Added Start and Update methods to octom class.

--- octom.cs
+++ octom.cs
@@ -6,11 +6,25 @@ using unityengine:
 // Create body:
 public class octom : MonoBehaviour {
 
+void Start()
+{
 
+}
+
+void Update()
+{
+
+
+}
+  
+void Animate()
+{
+
+}
+  
 
 // variables: 
-  
-    
+      
     public int flyingTime = 10.0f;
     public int flyEndTime = 13.0f;
 

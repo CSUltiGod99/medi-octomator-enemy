09ca5e9ff2ce2b8331926067f19ece363dd2cee9
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2026-04-24 16:05:16 +0100

    Update enemy movement logic and force application
    Fixed Rigidbody force application and adjusted flying time logic.

--- enemyMovement.cs
+++ enemyMovement.cs
@@ -1,4 +1,5 @@
 // Octomator enemy manager:
+// source code:
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine:
@@ -10,27 +11,21 @@ public class enemyMove : MonoBehaviour {
     public int flyEndTime = 13.0f;
 
     public Rigidbody _rigidbody;
-
-    
   
   void Start()
   {
      _rigidbody = GetComponent<Rigidbody>();
-    flyingTime = 11.0f;
+      flyingTime = 11.0f;
   }
 
   void Update()
   {
-    
-    
-       _rigidbody = GetRigidbody(); // Change this line its uses AddForce '_rigidbody.AddForce(0, 0, 9);
+     _rigidbody.AddForce(1, 0, 9);
+
+    if(flyingTime == 10f)
+    {
+        !flyingTime = new flyingTime 11.0f;
+        Debug.Log("Current flying time: ");
     }
   }
-  
-  void Animate()
-  {
-
-  }
-  
-
 }

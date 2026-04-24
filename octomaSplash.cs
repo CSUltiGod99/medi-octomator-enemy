cc986c4b42a307416018367c2ccee5cdf714bdac
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2026-04-24 15:42:45 +0100

    Create octomaSplash.cs

---
+++ octomaSplash.cs
@@ -0,0 +1,24 @@
+// this octomaSplash - This will make the Octomator Spit Green Acid:
+// source code
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class octomaSplash : MonoBehaviour
+{
+    public GameObject octomator_1;
+    public ParticleSystem _acid;  // Particle System 
+    public float pacidCount = 3.0f;
+    
+
+    
+    void Start()
+    {
+
+
+    }
+
+    void Update()
+    {
+      _acid = Count
+
